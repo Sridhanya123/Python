3138a865ce588ce06d179f7846f1cffd359f5d9a
ssridhanya2@gmail.com <ssridhanya2@gmail.com>
2025-12-30 15:40:42 +0530

    day1

---
+++ juiceshopday1cw.py
@@ -0,0 +1,10 @@
+apple= 15.5
+orange=20
+grapes=10.25
+total_volume= apple+orange+grapes
+print(total_volume)
+total_volume=int(total_volume)
+print(total_volume)
+print("The total volume of the juice sold is:",total_volume)
+import random
+print("Final total:",random.randrange(5, 10)+total_volume)
